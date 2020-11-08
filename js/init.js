09c690f677fc86f7a2f4311ad60b7600da690bf8
kml@kmldev.com <kamillo57@wp.pl>
2020-11-08 13:49:56 +0000

    Create tetrominoes and tetrominoes translation
    Tetrominoes position arrays has been created.
    For testing purpose tetrominoes has been displayed on game scene

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 let game=document.querySelector('.game-grid');
-function init()
+init=()=>
 {
     for(let i=0;i<200;i++)
     {
--- js/tetris.js
+++ js/tetris.js
@@ -1,6 +1,77 @@
-let gameSquers = Array.from(document.querySelectorAll('.game-grid div'));
-console.log(gameSquers)
 
+const score = document.getElementById("#score");
+const startButton= document.getElementById('#startBtn');
+let gameSquareArray = Array.from(document.querySelectorAll('.game-grid div'));
+
+// two translation
+const iTetromino=
+[
+    [-1,0,1,2],
+    [1,11,21,31],
+    
+];
+// one translation
+const oTetromino=
+[
+    [0,1,10,11],
+    
+];
+// four translation
+const tTetromino=
+[
+    [0,1,2,11],
+    [1,11,21,12],
+    [1,11,21,10],
+    [1,10,11,12]
+];
+// four translation
+const lTetromino=
+[
+    
+    [0,1,11,21],
+    [2,10,11,12],
+    [0,10,20,21],
+    [0,1,2,10]
+    
+];
+// four translation
+const jTetromino=
+[
+    [0,10,20,19],
+    [0,1,2,12],
+    [0,10,20,1],
+    [0,10,11,12]
+    
+];
+// two translation
+const sTetromino=
+[
+    [0,1,9,10],
+    [0,10,11,21],
+    
+];
+// two translation
+const zTetromino=
+[
+    [0,1,11,12],
+    [1,11,10,20],
+    
+];
+
+const theTetrominoes=[iTetromino,oTetromino,tTetromino,lTetromino,jTetromino,sTetromino,zTetromino];
+let randomTetromino =Math.floor(Math.random()*theTetrominoes.length);
+
+let currentPosition = 4;
+let current = theTetrominoes[randomTetromino][0];
+
+function draw() {
+
+    current.forEach(index => {
+        gameSquareArray[currentPosition + index].classList.add('tetrominos');
+    })
+}
+
+draw();
 
 
 
